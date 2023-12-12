96b6f0adb7a141277f5cb53323b0bd9b211204aa
aro-freelance <aro.freelance@gmail.com>
2023-12-12 11:51:31 -0500

    AbilitySystemComponent and AttributeSet Constructors
    3.21 Constructed the component to control the ability system on enemies and for the player. Set up to function for multiplayer by using SetReplicationMode Mixed for the player. Also constructed attributeset for enemy and player.

--- Aura/Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
+++ Aura/Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -6,9 +6,6 @@
 #include "AbilitySystemComponent.h"
 #include "AuraAbilitySystemComponent.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class AURA_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
 {
--- Aura/Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Aura/Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -6,9 +6,7 @@
 #include "AttributeSet.h"
 #include "AuraAttributeSet.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
