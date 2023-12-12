eaeb1c26690e78d02efcaa39b0b8fe3bc1aa34be
aro-freelance <aro.freelance@gmail.com>
2023-12-12 17:16:09 -0500

    Init Ability Actor Info
    3.23 Initialize the Ability Actor for both the player and the enemy.

--- Aura/Source/Aura/Public/Character/AuraEnemy.h
+++ Aura/Source/Aura/Public/Character/AuraEnemy.h
@@ -17,7 +17,14 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 
 public:
 	AAuraEnemy();
+
+	/////Enemy Interface////////
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+	//////////////////////////
+	///
+
+protected:
+	virtual void BeginPlay() override;
 	
 };
