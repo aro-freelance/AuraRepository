c23f1233dad943f1859f5b77e136b573c3e571ce
aro-freelance <aro.freelance@gmail.com>
2024-03-06 13:32:12 -0500

    Character Class Data Assets
    127, 128, 129
    CharacterClassInfo class.
    Character Class Data Assets.

---
+++ Aura/Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Arora Software
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
